486df4b78ca38b9459d934304d26a1d1763ab86f
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-01-06 09:00:50 -0300

    Initial setup

---
+++ src/main/java/com/saadeh/democassandra/model/entities/Department.java
@@ -0,0 +1,33 @@
+package com.saadeh.democassandra.model.entities;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Table(value = "departments")
+public class Department {
+
+    @PrimaryKey
+    private UUID id;
+    private String name;
+
+    public Department() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
