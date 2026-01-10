95d5c3df92059f03c15e277189b24a64dd37f69b
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-01-10 10:05:40 -0300

    Department findById, exceptionHandler

--- src/main/java/com/saadeh/democassandra/controllers/DepartmentController.java
+++ src/main/java/com/saadeh/democassandra/controllers/DepartmentController.java
@@ -5,10 +5,12 @@ import com.saadeh.democassandra.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -22,5 +24,11 @@ public class DepartmentController {
     public ResponseEntity<List<DepartmentDTO>> findAll(){
         List<DepartmentDTO> list = service.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id){
+        DepartmentDTO result = service.findById(id);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/saadeh/democassandra/services/DepartmentService.java
+++ src/main/java/com/saadeh/democassandra/services/DepartmentService.java
@@ -3,10 +3,14 @@ package com.saadeh.democassandra.services;
 import com.saadeh.democassandra.model.dto.DepartmentDTO;
 import com.saadeh.democassandra.model.entities.Department;
 import com.saadeh.democassandra.repositories.DepartmentRepository;
+import com.saadeh.democassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DepartmentService {
@@ -17,4 +21,12 @@ public class DepartmentService {
         List<Department> list = repository.findAll();
         return list.stream().map(DepartmentDTO::new).toList();
     }
+
+    public DepartmentDTO findById(UUID id){
+        Optional<Department> result = repository.findById(id);
+        Department entity = result.orElseThrow(()-> new ResourceNotFoundException("Id not found."));
+        return new DepartmentDTO(entity);
+    }
+
+
 }
